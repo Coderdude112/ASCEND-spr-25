18ae5f67d885c2186a4e78bdbbcf8dc33f61af5f
IntersellarDev7 <57544870+InterstellarDev7@users.noreply.github.com>
2025-02-24 19:20:10 -0700

    Get data from BNO055 and print to Serial and .csv (Testing Code)

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SD.h>
+#include <Adafruit_BNO055.h>
 
 // ---- //
 /* Vars */
 // ---- //
 
 File dataFile; //Instance of the File class
+Adafruit_BNO055 bno; //Instance of the Adafruit_BNO055 class
 
 // ---- //
 /* Main */
@@ -44,8 +46,71 @@ void setup() {
       else{
         Serial.println("Error opening flightdata.csv");
       }
+
+    /* Initialize the BNO055 */
+    if(!bno.begin()){
+        Serial.print("There was a problem detecting the BNO055 ... check your wiring or I2C ADDR!");
+        while(1);
+    }
 }
 
 void loop() {
     
+    // Get the Euler angles, quaternion, and temperature //
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    imu::Quaternion quat = bno.getQuat();
+    int8_t temp = bno.getTemp();
+
+    // Print the data to the .csv file //
+    dataFile = SD.open("flightdata.csv", FILE_WRITE);
+    if(dataFile){
+        dataFile.print(euler.x()); dataFile.print(", ");
+        dataFile.print(euler.y());
+        dataFile.print(", ");
+        dataFile.print(euler.z());
+        dataFile.print(", ");
+        dataFile.print(quat.w());
+        dataFile.print(", ");
+        dataFile.print(quat.x());
+        dataFile.print(", ");
+        dataFile.print(quat.y());
+        dataFile.print(", ");
+        dataFile.print(quat.z());
+        dataFile.print(", ");
+        dataFile.print(temp);
+        dataFile.println();
+        dataFile.close();
+    }
+    else{
+        Serial.println("Error opening flightdata.csv");
+    }
+    
+    /* Testing Code for BNO055 */
+    // Absolute Orientation (Euler Vector, 100Hz) //
+    Serial.print("X: ");
+    Serial.print(euler.x());
+    Serial.print(" Y: ");
+    Serial.print(euler.y());
+    Serial.print(" Z: ");
+    Serial.print(euler.z());
+    Serial.println("");
+   
+    // Absolute Orientation (Quaterion, 100Hz) //
+    Serial.print("qW: ");
+    Serial.print(quat.w(), 4);
+    Serial.print(" qX: ");
+    Serial.print(quat.x(), 4);
+    Serial.print(" qY: ");
+    Serial.print(quat.y(), 4);
+    Serial.print(" qZ: ");
+    Serial.print(quat.z(), 4);
+    Serial.println("");
+
+    // Temperature (1Hz) //
+    Serial.print("Current Temperature: ");
+    Serial.print(temp);
+    Serial.println(" C");
+    Serial.println("");
+
+    delay(100);
 }
