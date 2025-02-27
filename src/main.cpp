7be4b3259b428ae80b8eb2a474ef66c70531396e
NV237 <200006130+NV237@users.noreply.github.com>
2025-02-26 18:15:35 -0700

    start-up for the scd41 should be complete

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 // ------- //
 
 #include <Arduino.h>
-#include <Wire.h>
 #include <SensirionI2cScd4x.h>
+#include <Wire.h>
 
 // ---- //
 /* Vars */
 // ---- //
+
+SensirionI2cScd4x scd41;
 
 // ---- //
 /* Main */
@@ -18,9 +20,18 @@ void setup() {
     Serial.begin(9600);
     Serial.setTimeout(0);
     Wire.begin();
+    scd41.begin(Wire, SCD41_I2C_ADDR_62);
+    int scd41good = scd41.wakeUp();
+    if (scd41good == 0) {
+        Serial.println("Error waking up the SCD41 sensor.");
+    }
+    uint16_t scd41data;
+    uint16_t co2Concentration = 0;
+    float temperature = 0.0;
+    float relativeHumidity = 0.0;
 
 }
 
 void loop() {
-
+    
 }
